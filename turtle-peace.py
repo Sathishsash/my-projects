5253ad21e53e5f06f0ca0581f9a46e5d96188f14
Sathish Kumar <45067825+Sathishsash@users.noreply.github.com>
2019-07-06 06:17:50 +0530

    Update and rename turtle-peace to turtle-peace.py

---
+++ turtle-peace.py
@@ -0,0 +1,70 @@
+#first we need turtle to do the magic
+'''No! Not the real old ugly (sometimes cute-only the baby) kind of turtles. I am talking about the -magician-
+turtle-module'''
+
+import turtle    #this is to import the library. As Simple as that
+
+''' Everything here is pretty straight forward. Take a look at it. It will make sense'''
+
+turtle.circle(90)
+turtle.left(90)
+turtle.forward(180)
+turtle.backward(90)
+turtle.left(140)
+turtle.forward(90)
+turtle.backward(90)
+turtle.left(90)
+turtle.forward(90)
+turtle.penup()
+turtle.forward(200)
+turtle.pendown()
+
+turtle.circle(90)
+turtle.left(90)
+turtle.forward(90)
+turtle.right(90)
+turtle.forward(90)
+turtle.backward(90)
+turtle.left(130)
+turtle.forward(90)
+turtle.backward(180)
+turtle.left(90)
+turtle.penup()
+
+turtle.forward(250)
+turtle.pendown()
+turtle.circle(90)
+turtle.left(90)
+turtle.forward(180)
+turtle.backward(90)
+
+turtle.left(45)
+turtle.forward(90)
+turtle.backward(90)
+turtle.right(90)
+turtle.forward(90)
+
+
+'''P.S:: Support World Peace'''
+
+turtle.right(90)
+turtle.penup()
+turtle.forward(300)
+turtle.pendown()
+
+turtle.circle(90)
+turtle.left(90)
+turtle.penup()
+turtle.forward(90)
+turtle.pendown()
+turtle.forward(90)
+turtle.backward(90)
+turtle.left(90)
+turtle.forward(90)
+turtle.backward(90)
+turtle.right(45)
+turtle.forward(90)
+turtle.backward(180)
+
+
+'''Just copy and paste it in your IDE to see how it works'''
